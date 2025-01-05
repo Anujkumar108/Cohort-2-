b3fa7ed8a04057fffd765b91ed37890cef53fadd
Anuj kumar <141645976+Anujkumar108@users.noreply.github.com>
2025-01-05 19:19:39 +0530

    Update week1.js

--- week1.js
+++ week1.js
@@ -1,6 +1,18 @@
 /*
 WEEK 1 (javascript)
 
+1) compiled vs interpreted
+2) Let var and const 
+3) Data Types 
+4) Operators 
+5)Function
+6) IF Else 
+7) Loops 
+8) Maps
+9) Filter
+10) find 
+11) Sort 
+12) Callback
 
 
 */
