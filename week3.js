71133999f113df9068528f2e516e28457693308e
Anuj kumar <141645976+Anujkumar108@users.noreply.github.com>
2025-01-09 20:40:30 +0530

    Update week3.js

--- week3.js
+++ week3.js
@@ -1,3 +1,21 @@
+node js :
+Runtime Environment: Provides a runtime environment for executing JavaScript on the server-side.
+Server-Side Programming: Allows developers to write server-side code in JavaScript.
+Low-Level: Provides a low-level API for building web applications.
+Scalable: Enables building scalable web applications.
+Used for: Real-time web applications, API development, and server-side programming.
+ 
+Express.js
+Web Framework: A web framework built on top of Node.js.
+High-Level: Provides a high-level API for building web applications.
+Easy to Use: Simplifies the process of building web applications.
+Flexible: Allows developers to build web applications with a flexible and modular architecture.
+Used for: Building web applications, RESTful APIs, and web services.
+ 
+Key Differences
+Purpose: Node.js is a runtime environment, while Express.js is a web framework.
+Level of Abstraction: Node.js provides a low-level API, while Express.js provides a high-level API.
+Complexity: Node.js requires more manual configuration, while Express.js simplifies the process of building web application
 /*
  week 3.1 -
 
